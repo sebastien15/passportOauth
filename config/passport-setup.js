2a57df77e8e98dc0ef1ea4e4370223d60744617d
sebastien <ndase15ba@gmail.com>
2021-01-19 12:29:11 +0200

    callback functin is bringing profile information

--- config/passport-setup.js
+++ config/passport-setup.js
@@ -8,7 +8,8 @@ passport.use(
         callbackURL: 'http://localhost:3000/auth/google/redirect',
         clientID: process.env.CLIENT_ID,
         clientSecret: process.env.CLIENT_SECRET
-    }, ()=>{
-    //passport call back fnction
+    }, (accessToken, refreshToken, profile, done)=>{
+        //passport callback function
+        console.log(profile)
     })
 )
