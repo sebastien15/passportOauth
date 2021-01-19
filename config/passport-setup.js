f3eee100c0ef2680e3becd2cc6ce3e7a4a977f71
sebastien <ndase15ba@gmail.com>
2021-01-19 12:11:33 +0200

    redirect uri setup finished

--- config/passport-setup.js
+++ config/passport-setup.js
@@ -2,10 +2,13 @@ const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20');
 require('dotenv').config();
 
-passport.use(new GoogleStrategy({
-    // opitons ofr the google strategy
-    clientID: process.env.CLIENT_ID,
-    clientSecret: process.env.CLIENT_SECRET
-}), ()=>{
+passport.use(
+    new GoogleStrategy({
+        // opitons ofr the google strategy
+        callbackURL: 'http://localhost:3000/auth/google/redirect',
+        clientID: process.env.CLIENT_ID,
+        clientSecret: process.env.CLIENT_SECRET
+    }, ()=>{
     //passport call back fnction
-})
+    })
+)
